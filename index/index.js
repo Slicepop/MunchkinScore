338cea769385be2c1cf9555bff62df6fd59cd26e
Slicepop <altacckadenseeger@gmail.com>
2023-06-12 21:30:37 -0400

    index has a text to push to api then to screen.

--- index/index.js
+++ index/index.js
@@ -1,7 +1,18 @@
-const PubNub = require('pubnub');
+const ably = new Ably.Realtime("TGKJ4Q.jWjiiQ:v95PiPO-mxSjusDRiT12_8NNLCI8qqkqAmwVhzjV74Q");
 
-const pubnub = new PubNub({
-  publishKey: "myPublishKey",
-  subscribeKey: "mySubscribeKey",
-  userId: "myUniqueUserId",
+// Get the channel to publish and subscribe to
+const channel = ably.channels.get('chan1');
+
+// Function to publish a message
+async function publish() {
+  const text = document.getElementById("message").value;
+  await channel.publish('PlayerName', text);
+}
+
+// Subscribe to messages on the channel
+channel.subscribe('PlayerName', (message) => {
+  const playerNameElement = document.querySelector(".playerName");
+  if (playerNameElement) {
+    playerNameElement.innerHTML = message.data;
+  }
 });
